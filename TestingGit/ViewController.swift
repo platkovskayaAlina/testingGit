97ac0f3d36ad97f4f124bc0f2ba8d48f5e614c06
Alina <platkovskaya_alinka@mail.ru>
2023-02-12 12:32:57 +0300

    Initial Commit

---
+++ TestingGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingGit
+//
+//  Created by Алина Платковская on 12.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
